bc9e103d53b04d718a72948b9a1752b05dddb896
VictorSA7 <novotecno19@gmail.com>
2023-07-08 11:17:57 +0200

    Initial commit

---
+++ es/formulario.php
@@ -0,0 +1,25 @@
+<?php
+/***VARIABLES POR GET ***/
+
+if (isset($_GET['lname'])) {
+  echo "Variable 'a' is set.<br>";
+  echo $_GET['lname'];
+}else{
+
+	echo "asgsdgfsdfgsdfg sdfg sdfg fsdg.<br>";
+	echo $_GET['lname'];
+}
+
+if (isset($_GET['fname'])) {
+  echo "Variable 'a' is set.<br>";
+  echo $_GET['fname'];
+}else{
+
+	echo "asgsdgfsdfgsdfg sdfg sdfg fsdg.<br>";
+	echo $_GET['fname'];
+}
+
+
+
+
+?>
